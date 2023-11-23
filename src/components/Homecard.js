3479b5021990c7b38f452bed15fbb3ca151b2c86
noah <101118381+nwyux@users.noreply.github.com>
2023-11-23 18:41:07 +0100

    hover home

--- src/components/Homecard.js
+++ src/components/Homecard.js
@@ -10,7 +10,7 @@ export default function Homecard() {
             <h1 className='text-5xl py-4 text-center sm:text-left md:text-3xl lg:text-4xl xl:text-6xl text-blanc'>SNIKRS</h1>
             <h2 className='text-xl text-center py-2 sm:text-left sm:text-md md:text-lg lg:text-2xl text-blanc'>NUMBER ONE <br/> ON THE <br/> SNEAKERS</h2>
             <div className='flex-row flex py-2 justify-center sm:justify-normal items-center gap-4'>
-                <Link to='/sneakers' className='bg-noir text-blanc py-2 px-4 rounded-lg text-xs'>DISCOVER</Link>
+                <Link to='/sneakers' className='bg-noir text-blanc py-2 px-4 rounded-lg text-xs hover:bg-zinc-800'>DISCOVER</Link>
                 <Link to='/about' className=' border-2 border-noir text-noir py-2 px-4 rounded-lg text-xs transition-all duration-150 hover:bg-noir hover:text-blanc'>ABOUT</Link>
             </div>
             </div>
--- src/components/NewProductItem.js
+++ src/components/NewProductItem.js
@@ -32,9 +32,9 @@ export default function ProductItem() {
           }}
         >
           {filterItems.map((item) => (
-            <SplideSlide key={item.id} className="w-ful  sm:max-w-none max-h-lg font-horizonbig">
-              <div className="flex justify-center items-center">
-                <div className="flex flex-col w-4/6 sm:w-auto justify-center items-center border-2 border-noir rounded">
+            <SplideSlide key={item.id} className="w-full sm:max-w-none max-h-lg font-horizonbig">
+              <div className="flex justify-center items-center ">
+                <div className="flex flex-col w-4/6 sm:w-auto justify-center items-center border-2 border-noir rounded hover:bg-zinc-200">
                   <Link onClick={() => window.top(0,0)} to={`/sneakers/${item.id}`} className="sm:w-80">
                     <img
                       src={item.img}
@@ -44,7 +44,7 @@ export default function ProductItem() {
                   </Link>
 
                   <div className="flex flex-col justify-center items-center">
-                    <h1 className="text-sm text-center sm:text-xl font-bold">{item.name}</h1>
+                    <h1 className="text-sm text-center sm:text-xl font-bold hover:underline">{item.name}</h1>
                     <h2 className="text-md text-zinc-500 sm:text-lg">
                       {item.category}'s shoes
                     </h2>
@@ -54,10 +54,10 @@ export default function ProductItem() {
                   </div>
 
                   <div className="flex justify-center items-center gap-2 py-2">
-                    <button className="bg-noir text-blanc py-2 px-2 sm:px-4 rounded-lg text-xs">
+                    <button className="bg-noir text-blanc py-2 px-2 sm:px-4 rounded-lg text-xs hover:bg-zinc-700">
                       ADD TO CART
                     </button>
-                    <button className="border-2 border-noir text-noir py-2 px-2 sm:px-4 rounded-lg text-xs transition-all duration-150 hover:bg-noir hover:text-blanc">
+                    <button className="border-2 border-noir hover:no-underline text-noir py-2 px-2 sm:px-4 rounded-lg text-xs transition-all duration-150 hover:bg-noir hover:text-blanc">
                       BUY NOW
                     </button>
                   </div>
